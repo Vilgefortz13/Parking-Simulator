7ec9cfa435edbc001dc9064148900135c3582df7
Rinat Uzanzaev <wilax175@gmail.com>
2022-07-09 17:06:30 +0600

    added month loop

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -6,6 +9,12 @@ public class Main {
     static Car car = new Car();
     private static final List<Boolean> parkingPlaces = parking.getParkingPlaces();
     private static final Set<Car> cars = parking.getCars();
+
     public static void main(String[] args) {
+        LocalDateTime monthLater = LocalDateTime.now().plusDays(30);
+        while (monthLater.isAfter(LocalDateTime.now())) {
+            monthLater = monthLater.minusMinutes(5);
+        }
     }
+
 }
