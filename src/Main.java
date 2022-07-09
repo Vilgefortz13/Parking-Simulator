6ca3d5ecc2a1e2e1f1d32645a591fda319860598
Rinat Uzanzaev <wilax175@gmail.com>
2022-07-09 11:17:03 +0600

    initialize parking lists

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+    static Parking parking = new Parking();
+    static Car car = new Car();
+    private static final List<Boolean> parkingPlaces = parking.getParkingPlaces();
+    private static final Set<Car> cars = parking.getCars();
     public static void main(String[] args) {
 
     }
--- src/Parking.java
+++ src/Parking.java
@@ -4,13 +4,13 @@ import java.util.Set;
 
 public class Parking {
     private Set<Car> cars;
-    private final List<Boolean> parkingSpaces = new ArrayList<>();
+    private final List<Boolean> parkingPlaces = new ArrayList<>();
 
     public Set<Car> getCars() {
         return cars;
     }
 
-    public List<Boolean> getParkingSpaces() {
-        return parkingSpaces;
+    public List<Boolean> getParkingPlaces() {
+        return parkingPlaces;
     }
 }
