a60a4cdc3de5ebccf68f723c6cdbfe3f05fa7c8e
Rinat Uzanzaev <wilax175@gmail.com>
2022-07-09 11:04:34 +0600

    complete test state

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,50 @@
+import java.time.LocalDate;
+import java.util.concurrent.ExecutionException;
+
 public class Car {
     private Integer plateNumber;
+    private String state;
+    private LocalDate date;
+    private LocalDate time;
+    private State stateObj;
+
+    public Integer getPlateNumber() {
+        return plateNumber;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalDate getTime() {
+        return time;
+    }
+
+    public State getStateObj() {
+        return stateObj;
+    }
+
+    public void setStateObj(State stateObj) {
+        this.stateObj = stateObj;
+    }
+
+    public void onTheRoad() {
+        try {
+            this.stateObj.onTheRoad(this);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void toTheParking() {
+        try {
+            this.stateObj.toTheParking(this);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/State.java
+++ src/State.java
@@ -1,10 +1,30 @@
 public enum State {
     IN_PARKING("in the parking") {
+        @Override
+        public void onTheRoad(Car car) throws Exception {
+            car.setStateObj(State.IN_WAY);
+            System.out.println("The car pulled out of the parking lot");
+        }
+
+        @Override
+        public void toTheParking(Car car) throws Exception {
+            throw new Exception("The car is already in the parking lot.");
+        }
+
 
     },
 
     IN_WAY("on the way") {
+        @Override
+        public void onTheRoad(Car car) throws Exception {
+            throw new Exception("The car is already ot the road");
+        }
 
+        @Override
+        public void toTheParking(Car car) throws Exception {
+            car.setStateObj(State.IN_PARKING);
+            System.out.println("The car arrived at the parking lot");
+        }
     };
 
     private String value;
@@ -16,4 +36,8 @@ public enum State {
     State(String value) {
         this.value = value;
     }
+
+    public abstract void onTheRoad(Car car) throws Exception;
+
+    public abstract void toTheParking(Car car) throws Exception;
 }
