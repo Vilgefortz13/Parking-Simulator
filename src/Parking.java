9abdc639c1c8cced356aa3da5ba15e34788c1657
Rinat Uzanzaev <wilax175@gmail.com>
2022-07-09 18:23:47 +0600

    delete set of cars

--- src/Parking.java
+++ src/Parking.java
@@ -3,12 +3,8 @@ import java.util.List;
 import java.util.Set;
 
 public class Parking {
-    private Set<Car> cars;
     private final List<Boolean> parkingPlaces = new ArrayList<Boolean>();
 
-    public Set<Car> getCars() {
-        return cars;
-    }
 
     public List<Boolean> getParkingPlaces() {
         return parkingPlaces;
